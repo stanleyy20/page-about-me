0d44b808ef983802ad30556023403c1278815574
Dawid Stach <dstach20@gmail.com>
2022-08-06 13:34:39 +0200

    code refactoring and changing the appearance of the project section on the mobile devices

--- src/components/MyProjects/MyProjects.jsx
+++ src/components/MyProjects/MyProjects.jsx
@@ -2,46 +2,40 @@ import React from 'react';
 
 import bemCssModules from 'bem-css-modules';
 
-import { default as ProjectStyle } from './MyProjects.module.scss';
-import { Project } from '../ProjectItem/Project';
+import ProjectsStyle from './MyProjects.module.scss';
+import ProjectListStyle from './Project.module.scss';
+
+import { GIT_HUB } from '../../helpers/projectsData';
 
 import { PROJECTS } from '../../helpers/projectsData';
 
-const {
-    CALCULATOR,
-    GAME_SAPER,
-    REPO_SEARCH,
-    TO_DO_APP,
-    WEATHER,
-    BOOKS,
-    ROCK_PAPER_SCISSORS,
-    TRANSLATOR,
-    GIT_HUB,
-} = PROJECTS;
-
-const style = bemCssModules(ProjectStyle);
+const style = bemCssModules(ProjectsStyle);
+const styleList = bemCssModules(ProjectListStyle);
 
 const MyProjects = () => {
+    const projectList = PROJECTS.map((project) => (
+        <div className={styleList()}>
+            <a
+                className={styleList('link')}
+                href={project.URL}
+                target='_blank'
+                rel='noopener noreferrer'>
+                Link
+            </a>
+            <div className={styleList('img')}>
+                <i className={project.ICON}></i>
+            </div>
+            <h3 className={styleList('title-h3')}>{project.TITLE}</h3>
+        </div>
+    ));
+
     return (
         <section className={style()} id='project'>
             <h2 className={style('title-h2')}>
                 My projects <span></span>
             </h2>
             <p className={style('text')}>Here you will find a list of my sample projects</p>
-            <div className={style('wrapper')}>
-                <Project Url={BOOKS.URL} Icon={BOOKS.ICON} Title={BOOKS.TITLE} />
-                <Project Url={TRANSLATOR.URL} Icon={TRANSLATOR.ICON} Title={TRANSLATOR.TITLE} />
-                <Project
-                    Url={ROCK_PAPER_SCISSORS.URL}
-                    Icon={ROCK_PAPER_SCISSORS.ICON}
-                    Title={ROCK_PAPER_SCISSORS.TITLE}
-                />
-                <Project Url={WEATHER.URL} Icon={WEATHER.ICON} Title={WEATHER.TITLE} />
-                <Project Url={REPO_SEARCH.URL} Icon={REPO_SEARCH.ICON} Title={REPO_SEARCH.TITLE} />
-                <Project Url={TO_DO_APP.URL} Icon={TO_DO_APP.ICON} Title={TO_DO_APP.TITLE} />
-                <Project Url={CALCULATOR.URL} Icon={CALCULATOR.ICON} Title={CALCULATOR.TITLE} />
-                <Project Url={GAME_SAPER.URL} Icon={GAME_SAPER.ICON} Title={GAME_SAPER.TITLE} />
-            </div>
+            <div className={style('wrapper')}>{projectList}</div>
             <p className={style('text', { botoom: true })}>
                 You can find all my projects on{' '}
                 <span>
--- src/helpers/projectsData.js
+++ src/helpers/projectsData.js
@@ -1,57 +1,52 @@
-export const PROJECTS = {
-    WEATHER: {
+export const PROJECTS = [
+    {
+        TITLE: 'Rock Paper Scissors',
+        ICON: 'fa-solid fa-hand-scissors',
+        URL: 'https://stanleyy20.github.io/rock-paper-scissors/',
+    },
+    {
+        TITLE: 'Search books',
+        ICON: 'fa-solid fa-book',
+        URL: 'https://stanleyy20.github.io/search-books/',
+    },
+    {
+        TITLE: 'Translator',
+        ICON: 'fa-solid fa-language',
+        URL: 'https://github.com/stanleyy20/translator-react',
+    },
+    {
         TITLE: 'Weather App',
         ICON: 'fa-solid fa-cloud-sun',
         URL: 'https://stanleyy20.github.io/weather-app/',
     },
 
-    REPO_SEARCH: {
+    {
         TITLE: 'Repository search',
         ICON: 'fa-solid fa-magnifying-glass',
         URL: 'https://stanleyy20.github.io/repository-search-github/',
     },
 
-    TO_DO_APP: {
+    {
         TITLE: 'To do App',
         ICON: 'fa-solid fa-calendar-check',
         URL: 'https://stanleyy20.github.io/to-do-app/',
     },
 
-    CALCULATOR: {
+    {
         TITLE: 'Calculator ',
         ICON: 'fa-solid fa-calculator',
         URL: 'https://stanleyy20.github.io/calculator-like-in-windows/',
     },
 
-    GAME_SAPER: {
+    {
         TITLE: 'Game Saper',
         ICON: 'fa-solid fa-chess-board',
         URL: 'https://stanleyy20.github.io/game-saper/',
     },
+];
 
-    GAME_BLACK_JACK: {
-        TITLE: 'Game Black Jack',
-        ICON: 'fa-solid fa-diamond',
-        URL: 'https://stanleyy20.github.io/game-black-jack/',
-    },
-    ROCK_PAPER_SCISSORS: {
-        TITLE: 'Rock Paper Scissors',
-        ICON: 'fa-solid fa-hand-scissors',
-        URL: 'https://stanleyy20.github.io/rock-paper-scissors/',
-    },
-    BOOKS: {
-        TITLE: 'Search your favorite books',
-        ICON: 'fa-solid fa-book',
-        URL: 'https://stanleyy20.github.io/search-books/',
-    },
-    TRANSLATOR: {
-        TITLE: 'Translator',
-        ICON: 'fa-solid fa-language',
-        URL: 'https://github.com/stanleyy20/translator-react',
-    },
-    GIT_HUB: {
-        TITLE: 'GitHub',
-        ICON: 'fa fa-github',
-        URL: 'https://github.com/stanleyy20',
-    },
+export const GIT_HUB = {
+    TITLE: 'GitHub',
+    ICON: 'fa fa-github',
+    URL: 'https://github.com/stanleyy20',
 };
